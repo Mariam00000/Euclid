04895b00ca4f51b44ce298ebca4fb644eb7cd0c6
Mariam <mariamgillani963@gmail.com>
2021-11-15 23:37:08 +0500

    first commit

---
+++ program.java
@@ -0,0 +1,7 @@
+class sum
+{ublic static void main(string args[]0
+{int a=3, b=6,c=7;
+c=a+b;
+system.out.println("The sumis:" +c);
+}
+}
---
+++ sum.java
@@ -0,0 +1,7 @@
+class sum
+{public static void main(string args[])
+{int a=3, b=6,c=7;
+c=a+b;
+system.out.println("The sumis:" +c);
+}
+}
